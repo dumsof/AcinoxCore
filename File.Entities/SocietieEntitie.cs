bf1b79a0c695edbcbecfe314da8bca8eeb0179b3
Darwin <darwinu34@yahoo.es>
2020-09-16 08:34:50 -0500

    Add project files.

---
+++ File.Entities/SocietieEntitie.cs
@@ -0,0 +1,10 @@
+﻿namespace File.Entities
+{
+    public class SocietieEntitie
+    {
+        public string Cod { get; set; }
+        public string Razons { get; set; }
+        public string Nif { get; set; }
+        public string CodMoneda { get; set; }
+    }
+}
---
+++ LoggerService/ILoggerManager.cs
@@ -0,0 +1,13 @@
+﻿namespace LoggerService
+{
+    public interface ILoggerManager
+    {
+        public void LogInfo(string message);
+
+        public void LogWarn(string message);
+
+        public void LogDebug(string message);
+
+        public void LogError(string message);
+    }
+}
