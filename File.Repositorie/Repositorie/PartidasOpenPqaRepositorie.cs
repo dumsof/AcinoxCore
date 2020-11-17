f587acee7bfb9fd2f524547ca85abb6379cf47de
Darwin <darwinu34@yahoo.es>
2020-11-17 15:19:23 -0500

    se ajusta la consulta de partidas compensadas para saber si una factura no esta paga en su totalidad.

--- File.Repositorie/Repositorie/PartidasOpenPqaRepositorie.cs
+++ File.Repositorie/Repositorie/PartidasOpenPqaRepositorie.cs
@@ -22,26 +22,26 @@
             //string consultaTemp = this.configurationQuerySql.Value.ConsultaSQLPartidasAbiertas;
             string querySQL = @$"SELECT DISTINCT
 		                                 codcli=CLI.RFC
-		                                ,ndoc=SerieDocumento+'-'+LTRIM(STR( NumeroDocumento))
+		                                ,ndoc=SerieDocumento+'-'+LTRIM(STR( NumeroDocumento)) 
 		                                ,nvcto=''
 		                                ,fchemi=(SELECT FORMAT (CA.FechaDocumento, 'yyyy-MM-dd'))
 		                                ,fchvcto=(SELECT FORMAT (CA.FechaVencimiento, 'yyyy-MM-dd'))
-		                                ,importe=CASE MO.CodigoMoneda
-				                                 WHEN  'USD' THEN  CA.TTotal
-				                                 ELSE  CAST((CA.TTotal/C.ValorCambio) AS DECIMAL (12,2))
-				                                 END
-		                                ,estado=CASE WHEN CA.FechaVencimiento>GETDATE() THEN
-							                                 0
-					                                 WHEN CA.FechaVencimiento<GETDATE() AND DATEADD(day,CA.DiasCredito,CA.FechaDocumento)<=GETDATE() THEN
-							                                 1
-					                                 ELSE
-							                                 2
-					                                 END
+		                                ,importe=CASE MO.CodigoMoneda 
+				                                    WHEN  'USD' THEN  CF.TTotal - CF.ImportePagado
+				                                    ELSE  CAST((CF.TTotal - CF.ImportePagado/C.ValorCambio) AS DECIMAL (12,2))
+				                                    END
+		                                ,estado=CASE WHEN CA.FechaVencimiento>GETDATE() THEN 
+							                                '0' 
+					                                    WHEN CA.FechaVencimiento<GETDATE() AND DATEADD(day,CA.DiasCredito,CA.FechaDocumento)<=GETDATE() THEN
+							                                '1'
+					                                    ELSE
+							                                '2'
+					                                    END
 		                                ,dotada=0
 		                                ,codvp=''
 		                                ,codcondp=LTRIM(STR(ISNULL(CP.ID_CondicionPago,0)))
 		                                ,codmondoc=MO.CodigoMoneda
-		                                ,impmondoc=CA.TTotal
+		                                ,impmondoc=CF.TTotal
 		                                ,ind1=''
 		                                ,ind2=''
 		                                ,ind3=''
@@ -52,16 +52,17 @@
 		                                ,ind8=''
 		                                ,ind9=''
 		                                ,tdoc=''
-		                                ,campoid=ISNULL(CLI.RFC,'')+'@#'+(SELECT FORMAT (CA.FechaDocumento, 'yyyy-MM-dd HH:MM:ss'))+'@#'+SerieDocumento+'-'+LTRIM(STR( NumeroDocumento))
+		                                ,campoid=ISNULL(CLI.RFC,'')+'@#'+(SELECT FORMAT (CA.FechaDocumento, 'yyyy-MM-dd HH:MM:ss'))+'@#'+SerieDocumento+'-'+LTRIM(STR( NumeroDocumento)) 
 		                                ,codejercicio=''
-		                                ,numdocorigen=''
-                                  FROM [Facturacion].[CXCCargos] CA
-                                  JOIN Corporativo.ClientesSucursales SU ON SU.ID_ClienteSucursal=CA.ID_ClienteSucursal
-                                  JOIN Corporativo.Clientes CLI ON CLI.ID_Cliente=SU.ID_Cliente
-                                  JOIN [Corporativo].[Monedas] MO ON MO.ID_Moneda=CA.ID_Moneda
-                                  LEFT JOIN Corporativo.ValoresCambios C ON C.ID_Moneda=1  AND C.FechaCambio=CAST(CA.FechaDocumento AS date)
-                                  LEFT JOIN Corporativo.CondicionesPagos CP ON CP.Dias=CA.DiasCredito
-                                  WHERE ISNULL(CA.Saldada,'')='' AND CA.TTotal>0  AND CLI.ID_Empresa={codEmpresa}";
+		                                ,numdocorigen=''	
+                                    FROM [Facturacion].[CXCCargos] CA
+                                    JOIN [Facturacion].[CFD] CF ON CF.ID_CFD=CA.ID_CFD
+                                    JOIN Corporativo.ClientesSucursales SU ON SU.ID_ClienteSucursal=CA.ID_ClienteSucursal
+                                    JOIN Corporativo.Clientes CLI ON CLI.ID_Cliente=SU.ID_Cliente
+                                    JOIN [Corporativo].[Monedas] MO ON MO.ID_Moneda=CA.ID_Moneda
+                                    LEFT JOIN Corporativo.ValoresCambios C ON C.ID_Moneda=1  AND C.FechaCambio=CAST(CA.FechaDocumento AS date)
+                                    LEFT JOIN Corporativo.CondicionesPagos CP ON CP.Dias=CA.DiasCredito
+                                    WHERE CF.Ttotal>CF.ImportePagado AND CLI.ID_Empresa={codEmpresa}";
 
             using (var result = this.GetAllExecuteReader(querySQL))
             {
